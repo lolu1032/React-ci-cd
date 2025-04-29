5eb5dc1c375eb6a000b7d349f685017f1383f381
lolu1032 <soowkw@naver.com>
2025-04-29 21:15:19 +0900

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CI CD 되나요123455
+          CI CD 되나요
         </p>
         <a
           className="App-link"
